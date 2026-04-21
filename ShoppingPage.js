a59c1522da9e6cd90b8ccd9b6fa9f9d5aa0e75b1
Anderson kwan Odelish <andersonkwan8510@gmail.com>
2026-04-21 14:12:55 +0700

    ganti bug

--- ShoppingPage.js
+++ ShoppingPage.js
@@ -2,7 +2,6 @@ var value = 1
 const banyak_produk = document.getElementById('inp_banyak_produk')
 function tambah_value(){
     value+=1
-
 }
 
-banyak_produk.innerHTML = value
+banyak_produk.innerHTML = value;
