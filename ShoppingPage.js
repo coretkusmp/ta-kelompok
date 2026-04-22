3a2fae42978b8d9a29619075efbe752a6687e5d7
Anderson kwan Odelish <andersonkwan8510@gmail.com>
2026-04-22 13:01:03 +0700

    tambah value

--- ShoppingPage.js
+++ ShoppingPage.js
@@ -1,7 +1,11 @@
-var value = 1
+
 const banyak_produk = document.getElementById("inp_banyak_produk");
 function tambah_value(){
-    value+=1
+    banyak_produk.value += 1;
 }
 
-banyak_produk.innerHTML = value;
+const banyak_produk = document.getElementById("inp_banyak_produk");
+function tambah_value(){
+    banyak+=1
+    banyak_produk.value = banyak;
+}
