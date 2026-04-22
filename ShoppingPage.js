2cfc8e75b53ab7e128196429763c734533a60239
Anderson kwan Odelish <andersonkwan8510@gmail.com>
2026-04-22 13:24:24 +0700

    bsadiiubhas9diubhasiudasiubd

--- ShoppingPage.js
+++ ShoppingPage.js
@@ -1,11 +1,13 @@
 
 
 const banyak_produk = document.getElementById("inp_banyak_produk");
-banyak_produk.value = 1
 function tambah_value(){
-    banyak_produk.value = banyak_produk.value + 1;
+    banyak_produk.value = parseInt(banyak_produk.value) + 1;
 }
 
 function kurang_value(){
-    banyak_produk.value -= 1;
+    if(banyak_produk.value > 1){
+        banyak_produk.value -= 1;
+    }
+
 }
