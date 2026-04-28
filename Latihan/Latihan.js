c589b66016beb99abe91fa61789ee844935ca193
PetaniTembakau <jimmy.006@ski.sch.id>
2026-04-28 14:06:45 +0700

    ig

--- Latihan/Latihan.js
+++ Latihan/Latihan.js
@@ -1,9 +1,15 @@
-// DOM SELECTION
+// // DOM SELECTION
 
 
 
-// getElementById
+// // getElementById
 
-// document.getElementById()
+// // document.getElementById()
 
-const judul = document.getElementById('judul');
+// // const judul = document.getElementById('judul');
+
+// const paragraf4 = document.querySelector('#b p');
+// paragraf4.style.color = 'red';
+// const li2 = document.querySelec
+// tor('section#b ul li:nth-child(2)')
+// li2.style.backgroundColor = 'red';
